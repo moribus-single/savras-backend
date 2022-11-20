8406c1427c87d29c854b6ce7646fef99bc84b6c6
favir0 <f4vir0@yandex.ru>
2022-11-20 16:59:22 +0700

    Исправление req.txt и добавление .env

--- core/database.py
+++ core/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL = "database_url"
+dbpass = config("DATABASE_PASSWORD")
+dbname = config("DATABASE_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{dbpass}@localhost/{dbname}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from core.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
+from core.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 app.include_router(get_all_routers())
