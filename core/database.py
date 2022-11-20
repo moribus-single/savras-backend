60cec32f0b0403d12259916a81f0c8ba08c657ce
thethebra <thethebra24@gmail.com>
2022-11-20 16:31:47 +0700

    Правки

--- core/database.py
+++ core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Maks24072001!@localhost/savras"
+SQLALCHEMY_DATABASE_URL = "database_url"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from core.router import get_all_routers
+from core.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
